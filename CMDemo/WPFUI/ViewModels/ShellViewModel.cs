74e5c326e50858ac1be3305629eba30eb2c0eaf0
Geno Enchev <gnenchev99@gmail.com>
2021-06-29 20:03:39 +0300

    Added data
    Also cleaned up some code

--- CMDemo/WPFUI/ViewModels/ShellViewModel.cs
+++ CMDemo/WPFUI/ViewModels/ShellViewModel.cs
@@ -2,13 +2,23 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using WPFUI.Models;
 
 namespace WPFUI.ViewModels
 {
     public class ShellViewModel : Screen
     {
         private string _firstName = "Tim";
+        private string _lastName;
+        private PersonModel _selectedPerson;
+        private BindableCollection<PersonModel> _people = new BindableCollection<PersonModel>();
 
+        public ShellViewModel()
+        {
+            People.Add(new PersonModel { FirstName = "Geno",  LastName = "Enchev" });
+            People.Add(new PersonModel { FirstName = "Ivan",  LastName = "Ivanov" });
+            People.Add(new PersonModel { FirstName = "Test",  LastName = "Test" });
+        }
         public string FirstName
         {
             get 
@@ -18,6 +28,41 @@ namespace WPFUI.ViewModels
             set 
             { 
                 _firstName = value;
+                NotifyOfPropertyChange(() => FirstName);
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set 
+            { 
+                _lastName = value;
+                NotifyOfPropertyChange(() => LastName);
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+       
+        public string FullName
+        {
+            get { return $"{FirstName} {LastName}"; }
+            
+        }
+
+        public BindableCollection<PersonModel> People
+        {
+            get { return _people; }
+            set { _people = value; }
+        }
+
+        public PersonModel SelectedPerson
+        {
+            get { return _selectedPerson; }
+            set
+            {
+                _selectedPerson = value;
+                NotifyOfPropertyChange(() => SelectedPerson);
             }
         }
 
